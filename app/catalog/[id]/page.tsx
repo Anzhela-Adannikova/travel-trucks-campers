70f2c391d3f86e1ff1cd519e08ca4cf7490d4218
Anzhela Adannikova <anzhelaadannikova@gmail.com>
2025-11-05 19:17:55 +0200

    add location and rating to catalog, cartTrack

--- app/catalog/[id]/page.tsx
+++ app/catalog/[id]/page.tsx
@@ -15,9 +15,32 @@ const CartDetails = async ({ params }: Props) => {
 
   return (
     <section className={css.detailSection}>
-      <h1 className={css.name}>{camper.name}</h1>
-      <p className={css.location}>{camper.location}</p>
-      <p className={css.price}>€{camper.price}</p>
+      <h3 className={css.name}>{camper.name}</h3>
+
+      {/* rating */}
+      <div className={css.meta}>
+        <div className={css.rating}>
+          <svg className={css.starIcon} width="16" height="16" aria-hidden>
+            <use href="/icons/icons.svg#icon-star" />
+          </svg>
+          <span className={css.ratingValue}>{camper.rating}</span>
+          <span className={css.reviewCount}>
+            ({camper.reviews?.length ?? 0} review)
+          </span>
+        </div>
+
+        {/* location */}
+        <div className={css.locationBlock}>
+          <svg className={css.mapIcon} width="16" height="16" aria-hidden>
+            <use href="/icons/icons.svg/#icon-Map" />
+          </svg>
+          <span className={css.location}>
+            {camper.location?.split(",").reverse().join(",")}
+          </span>
+        </div>
+      </div>
+
+      <p className={css.price}>€{camper.price.toFixed(2)}</p>
 
       <div className={css.img}>
         {Array.isArray(camper.gallery) && camper.gallery.length > 0 ? (
--- components/CartTrack/CartTrack.tsx
+++ components/CartTrack/CartTrack.tsx
@@ -25,10 +25,28 @@ const CartTrack = ({ item }: Props) => {
         <div className={css.info}>
           <div className={css.header}>
             <h3 className={css.name}>{item.name}</h3>
-            <p className={css.price}>€{item.price}</p>
+            <p className={css.price}>€{item.price.toFixed(2)}</p>
+            <svg className={css.likeHeart} width={26} height={24}>
+              <use href={"/icons/icons.svg#icon-heart"}></use>
+            </svg>
           </div>
 
-          {item.location && <p className={css.location}>{item.location}</p>}
+          <div className={css.rating}>
+            <svg className={css.starIcon} width="16" height="16" aria-hidden>
+              <use href="/icons/icons.svg#icon-star" />
+            </svg>
+            <span className={css.ratingValue}>{item.rating}</span>
+            <span className={css.reviewCount}>
+              ({item.reviews?.length ?? 0} review)
+            </span>
+
+            <svg className={css.mapIcon} width="16" height="16" aria-hidden>
+              <use href="/icons/icons.svg/#icon-Map" />
+            </svg>
+            <span className={css.location}>
+              {item.location?.split(",").reverse().join(",")}
+            </span>
+          </div>
 
           {item.description && (
             <p className={css.description}>{item.description}</p>
