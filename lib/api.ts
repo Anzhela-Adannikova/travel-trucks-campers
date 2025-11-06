2c86e5884bb60943799ea395ec5f3e1afcec3ceb
Anzhela Adannikova <anzhelaadannikova@gmail.com>
2025-11-06 11:24:12 +0200

    add filter and pagination

--- lib/api.ts
+++ lib/api.ts
@@ -1,10 +1,15 @@
-import { Campers, CampersResponse } from "@/types/campers";
+import { CamperFilter, Campers, CampersResponse } from "@/types/campers";
 import axios from "axios";
 
 axios.defaults.baseURL = "https://66b1f8e71ca8ad33d4f5f63e.mockapi.io";
 
-export const getCatalog = async (): Promise<CampersResponse> => {
-  const res = await axios.get<CampersResponse>("/campers");
+export const getCatalog = async (
+  page: number = 1,
+  limit: number = 4
+): Promise<CampersResponse> => {
+  const res = await axios.get<CampersResponse>("/campers", {
+    params: { page, limit },
+  });
 
   return res.data;
 };
@@ -14,10 +19,30 @@ export const getCartDetail = async (id: string) => {
   return res.data;
 };
 
-// const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+export const getEilterCampers = async (
+  filters?: CamperFilter,
+  page: number = 1,
+  limit: number = 4
+): Promise<CampersResponse> => {
+  const params: Record<string, string | boolean | number> =
+    // об’єкт, де ключ — це рядок, а значення може бути або рядком, або числом, або булевим значенням (true/false)
+    {
+      page,
+      limit,
+    };
 
-// export const getCart = async () => {
-//   await delay(2000);
-//   const res = await axios.get<CampersResponse>("/campers");
-//   return res.data;
-// };
+  if (filters?.location) params.location = filters.location;
+
+  if (filters?.AC) params.AC = true;
+  if (filters?.Automatic) params.Automatic = true;
+  if (filters?.Kitchen) params.Kitchen = true;
+  if (filters?.TV) params.TV = true;
+  if (filters?.Bathroom) params.Bathroom = true;
+
+  if (filters?.Van) params.Van = "van";
+  if (filters?.FullyIntegrated) params.FullyIntegrated = "fullyIntegrated";
+  if (filters?.Alcove) params.Alcove = "alcove";
+
+  const res = await axios.get<CampersResponse>("/campers", { params });
+  return res.data;
+};
--- lib/api/serverApi.ts
+++ lib/api/serverApi.ts
@@ -1,39 +1,9 @@
-// import { CamperFilter, Campers } from "@/types/campers";
-// import axios from "axios";
-
-// export const nextServer = axios.create({
-//   baseURL: process.env.NEXT_PUBLIC_API_URL,
-// });
-
-// export const getFiltersCamper = async (
-//   filters: CamperFilter
-// ): Promise<Campers[]> => {
-//   const { page = 1, limit = 4, ...rest } = filters;
-//   const params = new URLSearchParams();
-
-//   Object.entries(rest).forEach(([key, value]) => {
-//     if (value !== undefined && value !== false && value !== "") {
-//       params.append(key, String(value));
-//     }
-//   });
-//   params.append("page", String(page));
-//   params.append("limit", String(limit));
-
-//   const { data } = await nextServer.get<Campers[]>(
-//     `/campers?${params.toString()}`
-//   );
-//   return data;
-// };
 import { Campers } from "@/types/campers";
 import axios from "axios";
 
 axios.defaults.baseURL = "https://66b1f8e71ca8ad33d4f5f63e.mockapi.io";
+
 export const getCatalog = async () => {
   const response = await axios.get<Campers>("/campers");
   return response.data;
 };
-
-// export async function getCatalog(page: number): Promise<Campers[]> {
-//   const response = await axios.get<Campers[]>(`/campers?page=${page}&limit=4`);
-//   return response.data;
-// }
